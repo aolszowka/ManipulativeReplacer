63811f1aaba2928152858f6a6c92eb7c2b20339d
Ace Olszowka <aolszowka@gmail.com>
2020-10-19 12:31:22 -0600

    Removed unused usings

--- ManipulativeReplacer/AboutManipulativeReplacerBox.cs
+++ ManipulativeReplacer/AboutManipulativeReplacerBox.cs
@@ -1,12 +1,7 @@
 ﻿namespace ManipulativeReplacer
 {
     using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Drawing;
-    using System.Linq;
     using System.Reflection;
-    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     partial class AboutManipulativeReplacerBox : Form
