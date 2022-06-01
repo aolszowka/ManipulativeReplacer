a0a49990e2405e9c0041abad28f6ea915b4002f8
Ace Olszowka <aolszowka@gmail.com>
2022-05-31 21:35:22 -0600

    Upgrade to .NET Core / Add GitHub Actions (#17)
    Upgrade to .NET Core and Add GitHub Actions to build it.

--- ManipulativeReplacer/Properties/AssemblyInfo.cs
+++ ManipulativeReplacer/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,9 +10,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ace Olszowka")]
 [assembly: AssemblyProduct("ManipulativeReplacer")]
-[assembly: AssemblyCopyright("Copyright © Ace Olszowka 2014-2015")]
+[assembly: AssemblyCopyright("Copyright © Ace Olszowka 2014-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: SupportedOSPlatform("windows")] // Only Support Windows For Now
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
